d372c2f60b62604338d2c33e34a244b629631ad2
amelie <amelieoller@gmail.com>
2017-12-14 14:32:52 -0800

    Add redux store and bootstrap support

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
+import 'bootstrap/dist/css/bootstrap.css';
 import App from './containers/App';
+import reducer from './reducers'
+
+const store = createStore(
+   reducer, 
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+   applyMiddleware(thunk)
+)
 
 ReactDOM.render(
-   <App />,
+   <Provider store={store}>
+      <App />
+   </Provider>,
    document.getElementById('root')
 );
 
