384ddfed3298e8b44a31ea8cd90d75c8ade40b79
amelie <amelieoller@gmail.com>
2017-12-14 19:08:52 -0800

    Add create crop action

--- src/actions/cropActions.js
+++ src/actions/cropActions.js
@@ -1,13 +1,37 @@
-import fetch from 'isomorphic-fetch';
+import { resetCropForm } from './cropFormActions'
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export function fetchCrops() {
-   return dispatch => {
-      dispatch({ type: 'LOADING_CROPS' })
-      return fetch(`${API_URL}/crops`)
-         .then(response => response.json())
-         .then(crops => dispatch({type: 'GET_CROPS', crops}))
-         .catch(error => console.log(error))
-   }
+export const fetchCrops = () => {
+	return dispatch => {
+		return fetch(`${API_URL}/crops`)
+				.then(response => response.json())
+				.then(crops => dispatch({type: 'GET_CROPS', crops}))
+				.catch(error => console.log(error))
+	}
+}
+
+export const createCrop = crop => {
+	return dispatch => {
+		return fetch(`${API_URL}/crops`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(crop)
+		})
+			.then(response => response.json())
+			.then(crop => {
+				dispatch({type: 'CREATE_CROP', crop})
+				dispatch(resetCropForm())
+			})
+			.catch(error => console.log(error))
+	}
+}
+
+export const deleteCrop = id => {
+	return {
+		type: 'DELETE_CROP',
+		id
+	}
 }
