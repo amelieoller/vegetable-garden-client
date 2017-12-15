aea428890f8496eb384dbf344e5efaf0886dfdd4
amelie <amelieoller@gmail.com>
2017-12-14 19:14:27 -0800

    Add single crop card

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Crop = ({crop}) => (
-   <div className="CropCard">
-      <h3>{crop.name}</h3>
-      <p>Days to Maturity: {crop.days_to_maturity}</p>
-   </div>
-)
+const API_URL = process.env.REACT_APP_API_URL;
+
+class Crop extends Component {
+
+   render() {
+      const {id, name, days_to_maturity} = this.props.crop
+      
+      return (
+         <div className="CropCard">
+            <h3>{name}</h3>
+            <p>Days to Maturity: {days_to_maturity}</p>
+         </div>
+      )
+   }
+}
 
 export default Crop;
