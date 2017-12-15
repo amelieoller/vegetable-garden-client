0d40740e354d8acf81b7ed6c72ecae2d4e07d619
amelie <amelieoller@gmail.com>
2017-12-14 19:14:57 -0800

    Add delete action to each crop

--- src/components/Crop.js
+++ src/components/Crop.js
@@ -1,8 +1,25 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions/cropActions'
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 class Crop extends Component {
+
+   handleOnClick = (id) => {
+      return fetch(`${API_URL}/crops/${id}`, {
+         method: 'DELETE'
+      })
+         .then(response => { 
+            if (response.ok) {
+               this.props.actions.deleteCrop(id)
+            } else {
+               window.alert('Unable to delete')
+            }
+         })
+			.catch(error => console.log(error))
+   }
 
    render() {
       const {id, name, days_to_maturity} = this.props.crop
@@ -11,9 +28,21 @@ class Crop extends Component {
          <div className="CropCard">
             <h3>{name}</h3>
             <p>Days to Maturity: {days_to_maturity}</p>
+            <button className={`delete_crop_${id}`} onClick={() => this.handleOnClick(id)}>Delete</button>
          </div>
       )
    }
 }
 
-export default Crop;
+const mapStateToProps = (state, ownProps) => {
+   return {
+      crops: state.crops,
+      crop: ownProps.crop
+   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+   return {actions: bindActionCreators(actions, dispatch)};   
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Crop);
--- src/reducers/cropsReducer.js
+++ src/reducers/cropsReducer.js
@@ -4,6 +4,8 @@ export default function cropsReducer(state = [], action) {
       return action.crops
     case 'CREATE_CROP':
       return state.concat(action.crop)
+    case 'DELETE_CROP':
+      return state.filter(crop => crop.id !== action.id)
     default:
       return state
   }
