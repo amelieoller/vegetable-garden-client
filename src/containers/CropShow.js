b6964fa02ea8647e9e8d9e317ef7a7c1949c01c7
amelie <amelieoller@gmail.com>
2017-12-16 10:53:21 -0800

    Edit display of crop on crop show component

--- src/containers/CropShow.js
+++ src/containers/CropShow.js
@@ -1,10 +1,31 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import DateFormat from "../components/DateFormat";
+
 const CropShow = ({ crop }) => (
-  <div>
-    <h2>{crop.name}</h2>
-    <p>Days to Maturity: {crop.days_to_maturity}</p>
+  <div className="row">
+    <div className="col-md-3">
+      <img
+        src={crop.image_url}
+        className="square-image large-image"
+        alt={crop.name}
+      />
+    </div>
+    <div className="col-md-4">
+      <h2>{crop.name}</h2>
+      <p>Days to Maturity: {crop.days_to_maturity}</p>
+      <p>
+        Date Planted: <DateFormat date={crop.date_planted} />
+      </p>
+      <p>
+        This crop is <strong>{crop.active ? "" : "not"}</strong> active in your garden.
+      </p>
+    </div>
+    <div className="col-md-5">
+      <h2>More Information</h2>
+      <button className="btn btn-primary">More Information on this Crop</button>
+    </div>
   </div>
 );
 
