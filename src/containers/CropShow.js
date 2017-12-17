2316b8e5fcbb869474bbceaa2455a66a923f8ba4
amelie <amelieoller@gmail.com>
2017-12-17 11:52:58 -0800

    Impement object destructuring and move more info logic

--- src/containers/CropShow.js
+++ src/containers/CropShow.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import DateFormat from "../components/DateFormat";
+import CropInfo from "./CropInfo";
 
-const CropShow = ({ crop }) => (
-  <div className="row">
-    <div className="col-md-3">
-      <img
-        src={crop.image_url}
-        className="square-image large-image"
-        alt={crop.name}
-      />
-    </div>
-    <div className="col-md-4">
-      <h2>{crop.name}</h2>
-      <p>Days to Maturity: {crop.days_to_maturity}</p>
-      <p>
-        Date Planted: <DateFormat date={crop.date_planted} />
-      </p>
-      <p>
-        This crop is <strong>{crop.active ? "" : "not"}</strong> active in your garden.
-      </p>
-    </div>
-    <div className="col-md-5">
-      <h2>More Information</h2>
-      <button className="btn btn-primary">More Information on this Crop</button>
-    </div>
-  </div>
-);
+class CropShow extends Component {
+  render() {
+    const {
+      image_url,
+      name,
+      days_to_maturity,
+      date_planted,
+      active
+    } = this.props.crop;
+
+    return (
+      <div className="row">
+        <div className="col-md-3">
+          <img
+            src={image_url}
+            className="square-image large-image"
+            alt={name}
+          />
+        </div>
+        <div className="col-md-3">
+          <h2>{name}</h2>
+          <p>Days to Maturity: {days_to_maturity}</p>
+          <p>
+            Date Planted: <DateFormat date={date_planted} />
+          </p>
+          <p>
+            This crop is <strong>{active ? "" : "not"}</strong> active in your
+            garden.
+          </p>
+        </div>
+        <div className="col-md-6">
+          <CropInfo name={this.props.crop.name} />
+        </div>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const crop = state.crops.find(
