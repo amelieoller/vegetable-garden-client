77cdfb342dcc88fd5820b495a13bb6e8fc470a22
amelie <amelieoller@gmail.com>
2017-12-19 10:30:04 -0800

    Add maturity date to CropShow and ready information

--- src/components/CropShow.js
+++ src/components/CropShow.js
@@ -1,10 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 
 import DateFormat from "../components/DateFormat";
 import CropInfo from "../containers/CropInfo";
 
 const CropShow = ({ crop }) => {
+  const maturityDate = (date, daysToMaturity) => {
+    const maturityDate = new Date(date);
+    maturityDate.setDate(maturityDate.getDate() + daysToMaturity);
+    return maturityDate;
+  };
+
+  const pastDue = maturityDate => {
+    let message = "";
+    if (maturityDate < new Date()) {
+      message = "Your crop should be ready.";
+    } else {
+      message = "Your crop is not ready yet.";
+    }
+    return message;
+  };
+
   const { image_url, name, days_to_maturity, date_planted, active } = crop;
 
   return (
@@ -22,6 +38,15 @@ const CropShow = ({ crop }) => {
           This crop is <strong>{active ? "" : "not"}</strong> active in your
           garden.
         </p>
+        <p>
+          Maturity Date:{" "}
+          <strong>
+            <DateFormat
+              date={maturityDate(crop.date_planted, crop.days_to_maturity)}
+            />
+          </strong>
+        </p>
+        <p>{pastDue(maturityDate(crop.date_planted, crop.days_to_maturity))}</p>
       </div>
       <div className="col-md-6">
         <CropInfo name={crop.name} />
