465670245be615770afef9b033bbb0933fff2f64
Amelie Oller <amelieoller@gmail.com>
2019-03-09 18:28:25 -0800

    Fix 3 argument createStore error

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./components/App";
-import reducer from "./reducers";
+import reducers from "./reducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  reducers,
+  composeEnhancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
