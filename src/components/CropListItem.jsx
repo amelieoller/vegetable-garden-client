411c684a5da3c4cc4efae39b96d41d6662a0b679
Amelie Oller <amelieoller@gmail.com>
2019-03-13 10:50:54 -0700

    Add id to active crop checkbox to fix misassignment

--- src/components/CropListItem.jsx
+++ src/components/CropListItem.jsx
@@ -5,58 +5,60 @@ import PropTypes from 'prop-types';
 import DateFormat from './DateFormat';
 
 const CropListItem = ({
-  crop, match, deleteCrop, updateCropActive,
-}) => {
-  const {
+  crop: {
     imageUrl, name, datePlanted, daysToMaturity, id, active,
-  } = crop;
-
-  return (
-    <div className="CropCard col-12 col-sm-6 col-md-4 col-lg-3">
-      {imageUrl ? (
-        <div className="square" style={{ backgroundImage: `url(${imageUrl})` }} />
-      ) : (
-        <div
-          className="square"
-          style={{
-            backgroundImage:
-              'url(https://x.kinja-static.com/assets/images/logos/placeholders/default.png)',
+  },
+  crop,
+  match,
+  deleteCrop,
+  updateCropActive,
+}) => (
+  <div className="CropCard col-12 col-sm-6 col-md-4 col-lg-3">
+    {imageUrl ? (
+      <div className="square" style={{ backgroundImage: `url(${imageUrl})` }} />
+    ) : (
+      <div
+        className="square"
+        style={{
+          backgroundImage:
+            'url(https://x.kinja-static.com/assets/images/logos/placeholders/default.png)',
+        }}
+      />
+    )}
+    <h3>
+      <Link to={`/crops/${id}`}>{name}</Link>
+    </h3>
+    <p>
+      Days to Maturity:
+      {daysToMaturity}
+    </p>
+    <p>
+      Planted on
+      {' '}
+      <DateFormat date={new Date(datePlanted)} />
+    </p>
+    <div className="checkbox">
+      <label htmlFor={`crop-${crop.id}`}>
+        <input
+          id={`crop-${crop.id}`}
+          type="checkbox"
+          checked={active}
+          onChange={() => {
+            updateCropActive(crop);
           }}
         />
-      )}
-      <h3>
-        <Link to={`/crops/${id}`}>{name}</Link>
-      </h3>
-      <p>
-        Days to Maturity:
-        {daysToMaturity}
-      </p>
-      <p>
-        Planted on
         {' '}
-        <DateFormat date={new Date(datePlanted)} />
-      </p>
-      <div className="checkbox">
-        <label htmlFor="cropActive">
-          <input
-            id="cropActive"
-            type="checkbox"
-            checked={active}
-            onChange={() => updateCropActive(crop)}
-          />
-          {' '}
-          Active
-        </label>
-      </div>
-      <button type="submit" className="btn btn-danger margin-right" onClick={() => deleteCrop(id)}>
-        Delete
-      </button>
-      <Link to={`${match.url}/${id}/Edit`} className="btn btn-primary">
-        Edit
-      </Link>
+        Active
+      </label>
     </div>
-  );
-};
+    <button type="submit" className="btn btn-danger margin-right" onClick={() => deleteCrop(id)}>
+      Delete
+    </button>
+    <Link to={`${match.url}/${id}/Edit`} className="btn btn-primary">
+      Edit
+    </Link>
+  </div>
+);
 
 CropListItem.propTypes = {
   crop: PropTypes.shape({
